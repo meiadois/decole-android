ad1c6289193581183e913e78720380fadf0400b1
Davi Rocha Macedo Sousa <davirmsousa@hotmail.com>
2020-05-20 00:26:30 -0300

    ajustes na tela de account

--- app/src/main/java/br/com/meiadois/decole/presentation/user/account/binding/UserAccountData.kt
+++ app/src/main/java/br/com/meiadois/decole/presentation/user/account/binding/UserAccountData.kt
@@ -1,8 +1,13 @@
 package br.com.meiadois.decole.presentation.user.account.binding
 
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+
 data class UserAccountData(
+    @Bindable
     var name : String = "",
+    @Bindable
     var email : String = "",
     var newPassword : String = "",
     var confirmPassword : String = ""
-)
+) : BaseObservable()
