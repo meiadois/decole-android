4e2fbe81b9d8084cbdade9270d4f3fec11bd8afb
Davi Rocha Macedo Sousa <davirmsousa@hotmail.com>
2020-05-26 11:21:48 -0300

    corrigindo exibição do conteúdo do fragment top

--- app/src/main/java/br/com/meiadois/decole/presentation/user/partnership/PartnershipHomeTopFragment.kt
+++ app/src/main/java/br/com/meiadois/decole/presentation/user/partnership/PartnershipHomeTopFragment.kt
@@ -38,7 +38,8 @@ class PartnershipHomeTopFragment : Fragment(), KodeinAware {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this, factory).get(PartnershipHomeTopViewModel::class.java)
-
+        setContentVisibility(CONTENT_NONE)
+        setProgressBarVisibility(true)
     }
 
     override fun onResume() {
@@ -48,38 +49,38 @@ class PartnershipHomeTopFragment : Fragment(), KodeinAware {
 
     private fun init(){
         Coroutines.main {
-            toggleLoading(true)
             try{
                 val company : Company = viewModel.getUserCompany()
+                setContentVisibility(CONTENT_WITH_ACCOUNT)
                 showUserCompany(company)
             }catch (ex: ClientException){
-                if (ex.code == 404) showInviteToRegister()
+                if (ex.code == 404) setContentVisibility(CONTENT_NO_ACCOUNT)
             }catch (ex: Exception){
                 Log.i("Fragment_top_exception", ex.message!!)
             }finally {
-                toggleLoading(false)
+                setProgressBarVisibility(false)
             }
         }
     }
 
-    private fun toggleLoading(loading: Boolean) {
-        if (loading){
-            top_progress_bar?.visibility = View.VISIBLE
-        }else {
-            top_progress_bar?.visibility = View.GONE
-        }
+    private fun setProgressBarVisibility(visible: Boolean){
+        top_progress_bar.visibility = if (visible) View.VISIBLE else View.GONE
+    }
+
+    private fun setContentVisibility(contentMode: Int){
+        container_company_layout_account.visibility = if (contentMode == CONTENT_WITH_ACCOUNT) View.VISIBLE else View.GONE
+        container_company_layout_no_account.visibility = if (contentMode == CONTENT_NO_ACCOUNT) View.VISIBLE else View.GONE
     }
 
     private fun showUserCompany(company: Company){
-        container_company_layout_no_account.visibility = View.INVISIBLE
-        container_company_layout_account.visibility = View.VISIBLE
         text_partner_name.text = company.name
         text_partner_segment.text = company.segment?.name
         Glide.with(container_company_layout_account).load(company.thumbnail).apply(RequestOptions.circleCropTransform()).into(image_partner)
     }
 
-    private fun showInviteToRegister(){
-        container_company_layout_account.visibility = View.INVISIBLE
-        container_company_layout_no_account.visibility = View.VISIBLE
+    companion object{
+        const val CONTENT_NONE = 0
+        const val CONTENT_NO_ACCOUNT = 1
+        const val CONTENT_WITH_ACCOUNT = 2
     }
 }
