727d42bdcc996685ff50f0e3b4888a3397a2ad03
Carvalho <kfcarvalho.018@gmail.com>
2020-06-02 17:46:59 -0300

    removendo comentários

--- app/src/main/java/br/com/meiadois/decole/presentation/user/partnership/PartnershipHomeBottomFragment.kt
+++ app/src/main/java/br/com/meiadois/decole/presentation/user/partnership/PartnershipHomeBottomFragment.kt
@@ -95,22 +95,6 @@ class PartnershipHomeBottomFragment : Fragment(), KodeinAware {
         }
     }
 
-//    private fun setActiveMenuItem(itemId: Int){
-//        currentMenuItemActive = itemId
-//        ICON_MATCH_ID.let {
-//            bottom_bar.menu.findItem(it)
-//                .setIcon(if (itemId == it) R.drawable.ic_mdi_people_green else R.drawable.ic_mdi_people_white)
-//        }
-//        ICON_RECEIVED_ID.let {
-//            bottom_bar.menu.findItem(it)
-//                .setIcon(if (itemId == it) R.drawable.ic_mdi_move_to_inbox_green else R.drawable.ic_mdi_move_to_inbox_white)
-//        }
-//        ICON_SENT_ID.let {
-//            bottom_bar.menu.findItem(it)
-//                .setIcon(if (itemId == it) R.drawable.ic_mdi_grade_green else R.drawable.ic_mdi_grade_white)
-//        }
-//    }
-
     override fun onResume() {
         super.onResume()
         init()
